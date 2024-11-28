a21aa46420a37599b618686f9a8db53b5047a6e3
Curt Spark <git@cspark.dev>
2024-11-28 14:06:49 +0000

    Init task model and database schema setup

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,6 +9,9 @@ public func configure(_ app: Application) async throws {
     // Setup database connection
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
 
+    // Setup database schema
+    app.migrations.add(InitTask())
+
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     // register routes
