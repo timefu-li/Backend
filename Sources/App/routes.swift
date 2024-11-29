3e7ac772ae2fa672c072bfed9a795d90667850b7
Curt Spark <git@cspark.dev>
2024-11-29 12:43:42 +0000

    Implement tasks GET and POST

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,25 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+
+    app.get("tasks") { (req: Request) async throws -> [Task] in
+        guard let tasks: [Task] = try? await Task.query(on: req.db).all() else {
+            throw Abort(.custom(code: 200, reasonPhrase: "No tasks found"))
+        }
+
+        return tasks
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.post("tasks") { (req: Request) async throws -> Task in
+        guard let taskmodel: Task = try? req.content.decode(Task.self) else {
+            throw Abort(.custom(code: 422, reasonPhrase: "Request body sent by user is invalid"))
+        }
+        guard let taskcreated: () = try? await taskmodel.create(on: req.db) else {
+            throw Abort(.custom(code: 500, reasonPhrase: "Request body valid but unable to add new task to database"))
+        }
+
+        return taskmodel
     }
+
+
 }
